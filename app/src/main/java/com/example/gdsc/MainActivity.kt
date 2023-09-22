caae6e3b44328f418853ec0688510a0a00bd474b
leastzero <csong7177@naver.com>
2023-09-22 21:42:59 +0900

    soyeong week2

--- app/src/main/java/com/example/gdsc/MainActivity.kt
+++ app/src/main/java/com/example/gdsc/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.gdsc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_coffee)
+        val adapter = IntroductionAdapter()
+        val itemList = listOf(
+            Item(R.drawable.coffee1, "미키 딜라이트 콜드 브루","Mickey Delight Cold Brew"),
+            Item(R.drawable.coffee2, "콜드 브루","Cold Brew"),
+            Item(R.drawable.coffee3, "나이트로 바닐라 크림","Nitro Vanilla Cold Brew"),
+            Item(R.drawable.coffee4, "제주 비자림 콜드 브루","Jeju Forest Cold Brew"),
+            Item(R.drawable.coffee5, "제주 시트러스 허니 콜드 브루","Jeju Citrus Honey Cold Brew"),
+        )
+        recyclerView.adapter = adapter
+        adapter.submitList(itemList)
     }
 }
