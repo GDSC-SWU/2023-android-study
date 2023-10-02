9cb7d45a84f28b9ca1986925f9ac977da5abc754
leastzero <csong7177@naver.com>
2023-10-02 16:26:46 +0900

    soyeong week3

--- app/src/main/java/com/example/gdsc/MainActivity.kt
+++ app/src/main/java/com/example/gdsc/MainActivity.kt
@@ -2,23 +2,49 @@ package com.example.gdsc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.RelativeLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val frame: RelativeLayout by lazy { // activity_main의 화면 부분
+        findViewById(R.id.body_container)
+    }
+
+    private val bottomNagivationView: BottomNavigationView by lazy {
+        findViewById(R.id.bottom_navigation)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.rv_coffee)
-        val adapter = IntroductionAdapter()
-        val itemList = listOf(
-            Item(R.drawable.coffee1, "미키 딜라이트 콜드 브루","Mickey Delight Cold Brew"),
-            Item(R.drawable.coffee2, "콜드 브루","Cold Brew"),
-            Item(R.drawable.coffee3, "나이트로 바닐라 크림","Nitro Vanilla Cold Brew"),
-            Item(R.drawable.coffee4, "제주 비자림 콜드 브루","Jeju Forest Cold Brew"),
-            Item(R.drawable.coffee5, "제주 시트러스 허니 콜드 브루","Jeju Citrus Honey Cold Brew"),
-        )
-        recyclerView.adapter = adapter
-        adapter.submitList(itemList)
+        supportFragmentManager.beginTransaction().add(frame.id, FirstFragment()).commit()
+
+        bottomNagivationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_first -> {
+                    replaceFragment(FirstFragment())
+                    true
+                }
+
+                R.id.nav_second -> {
+                    replaceFragment(SecondFragment())
+                    true
+                }
+
+                R.id.nav_third -> {
+                    replaceFragment(ThirdFragment())
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(frame.id, fragment).commit()
     }
 }
