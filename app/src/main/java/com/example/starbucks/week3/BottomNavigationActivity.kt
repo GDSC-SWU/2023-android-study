08e603bdec040d01025e07d2eb0c01cd55a50338
yerim123456 <yearim1226@gmail.com>
2023-10-03 11:40:54 +0900

    feat: Add bottom navigation view

---
+++ app/src/main/java/com/example/starbucks/week3/BottomNavigationActivity.kt
@@ -0,0 +1,50 @@
+package com.example.starbucks.week3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.starbucks.R
+import com.example.starbucks.databinding.ActivityBottomNavigationBinding
+
+class BottomNavigationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBottomNavigationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 애플리케이션 실행 후 첫 화면 설정
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fl_bottom_naviation_layout, CalendarFragment())
+            .commit()
+
+        // 하단 네비게이션 바 클릭 이벤트 설정
+        binding.bnvBottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.nav_bottom_navigation_calendar -> {
+                    replaceFragment(CalendarFragment())
+                    true
+                }
+                R.id.nav_bottom_navigation_charts -> {
+                    replaceFragment(ChartFragment())
+                    true
+                }
+                R.id.nav_bottom_navigation_comments -> {
+                    replaceFragment(CommentFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    // 화면 전환 구현 메소드
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fl_bottom_naviation_layout, fragment)
+            .commit()
+    }
+}
